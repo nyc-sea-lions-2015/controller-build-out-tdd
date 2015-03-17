50b54d9337e472beab9ee36f74910233a24f9bd1
Dan Asselin <dasselin86@gmail.com>
2015-03-17 10:36:52 -0400

    Rebasing

--- source/spec/controllers/index_spec.rb
+++ source/spec/controllers/index_spec.rb
@@ -1,23 +1,41 @@
 require_relative '../spec_helper'
 
-describe 'Index Controller' do 
+describe 'Index Controller' do
 
-it 'Should get the / route' do
-    get '/' 
-    expect(last_response).to be_ok
-    expect(last_response.body).to include('Welcome to Pier 93')
-end
+  it 'Should get the / route' do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('Welcome to Pier 93')
+  end
 
-it 'Should get the /another route' do
-    get '/schedule' 
-    expect(last_response).to be_ok
-    expect(last_response.body).to include('We have a lot to do today!')
-end
+  it 'Should get the /another route' do
+      get '/schedule'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('We have a lot to do today!')
+  end
 
-it 'Should get the /dinner route' do
-    get '/dinner' 
-    expect(last_response).to be_ok
-    expect(last_response.body).to include('Dinner Bell! Dinnnnngggg')
+  it 'Should get the /dinner route' do
+      get '/dinner'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('Dinner Bell! Dinnnnngggg')
+  end
 end
+# it 'Should verify that the response is a redirect' do
+#   post '/try_a_post'
+#   expect(last_response).to be_redirect
+#   follow_redirect!
+#   expect(last_response).to be_ok
+#   expect(last_response.body).to include('Welcome to Pier 93')
+# end
 
-end
+# it 'Should verify redirect' do
+#   put '/try_a_put'
+#   expect(last_response).to be_redirect
+#   follow_redirect!
+#   expect(last_response).to be_ok
+#   expect(last_response.body).to include('Welcome to Pier 93')
+# end
+
+# it 'Should verify the parameters' do
+#   put '/try_a_put' :params{with_a_parameter => true}
+# end
