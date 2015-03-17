0d09f775142015ae43e0de620add487f6e6e663f
Ben Cheng & Kurt Schnelker <pair+bcheng90.sabron13@devbootcamp.com>
2015-03-17 11:03:50 -0400

    passes release 0

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,3 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+
 get '/' do
-  File.read(File.join('public','index.html')) 
+  File.read(File.join('public','index.html'))
+
 end
+
+get '/index2' do
+  File.read(File.join('public','index_2.html'))
+end
+
+get '/index3' do
+  File.read(File.join('public','index_3.html'))
+end
+
--- source/spec/controllers/index_spec.rb
+++ source/spec/controllers/index_spec.rb
@@ -1,21 +1,34 @@
-require_relative '../spec_helper'
+require './app/controllers/index'
+require './spec/spec_helper'
 
-describe 'Index Controller' do 
+describe 'Index Controller' do
 
 it 'Should get the / route' do
-    get '/' 
+    get '/'
     expect(last_response).to be_ok
     expect(last_response.body).to include('Welcome to Pier 93')
 end
 
+it 'Should get the /index2 route' do
+    get '/index2'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('This is the second index page')
+end
+
+it 'Should get the /index3 route' do
+    get '/index3'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include(' This is the third index page')
+end
+
 it 'Should get the /another route' do
-    get '/schedule' 
+    get '/schedule'
     expect(last_response).to be_ok
     expect(last_response.body).to include('We have a lot to do today!')
 end
 
 it 'Should get the /dinner route' do
-    get '/dinner' 
+    get '/dinner'
     expect(last_response).to be_ok
     expect(last_response.body).to include('Dinner Bell! Dinnnnngggg')
 end
