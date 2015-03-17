9d32792e4862ca0735264413aff76a512bb3f60e
Dan Asselin & Eveanandi Butler <pair+danasselin.ebutler90@devbootcamp.com>
2015-03-17 10:43:44 -0400

    Revise File#join method in index.rb to find correct directory

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,3 +1,5 @@
 get '/' do
-  File.read(File.join('public','index.html')) 
+  File.read(File.join('..', '..', 'public','index.html'))
+  # Above we added path parameters '..' to find the right directory to open
+  # File.read('../../public/index.html')
 end
