aa3a2dbe9446c5e6a2c60284f0b7053019a08293
Dan Asselin <dasselin86@gmail.com>
2015-03-17 10:55:02 -0400

    Finish Rspec Tests

--- source/spec/controllers/index_spec.rb
+++ source/spec/controllers/index_spec.rb
@@ -19,23 +19,28 @@ describe 'Index Controller' do
       expect(last_response).to be_ok
       expect(last_response.body).to include('Dinner Bell! Dinnnnngggg')
   end
+
+  it 'Should verify that the response is a redirect' do
+    post '/try_a_post'
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Welcome to Pier 93')
+  end
+
+  it 'Should verify redirect' do
+    put '/try_a_put'
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Welcome to Pier 93')
+  end
+
+  it 'Should verify the parameters' do
+    put '/try_a_put', :params=>{with_a_param: true}
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Welcome to Pier 93')
+  end
 end
-# it 'Should verify that the response is a redirect' do
-#   post '/try_a_post'
-#   expect(last_response).to be_redirect
-#   follow_redirect!
-#   expect(last_response).to be_ok
-#   expect(last_response.body).to include('Welcome to Pier 93')
-# end
-
-# it 'Should verify redirect' do
-#   put '/try_a_put'
-#   expect(last_response).to be_redirect
-#   follow_redirect!
-#   expect(last_response).to be_ok
-#   expect(last_response.body).to include('Welcome to Pier 93')
-# end
-
-# it 'Should verify the parameters' do
-#   put '/try_a_put' :params{with_a_parameter => true}
-# end
