329f16e0edf0cb3ba3367133a8ae3dcfa90b8771
Max Rater & Ryan <pair+maxrater.rimmesbe@devbootcamp.com>
2015-03-17 10:26:18 -0400

    Finished Release 0

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,3 +1,12 @@
 get '/' do
-  File.read(File.join('public','index.html')) 
+  File.read(File.join('public','index.html'))
+  'Welcome to Pier 93'
+end
+
+get '/schedule' do
+  'We have a lot to do today!'
+end
+
+get '/dinner' do
+  'Dinner Bell! Dinnnnngggg'
 end
--- source/spec/controllers/index_spec.rb
+++ source/spec/controllers/index_spec.rb
@@ -1,21 +1,21 @@
 require_relative '../spec_helper'
 
-describe 'Index Controller' do 
+describe 'Index Controller' do
 
 it 'Should get the / route' do
-    get '/' 
+    get '/'
     expect(last_response).to be_ok
     expect(last_response.body).to include('Welcome to Pier 93')
 end
 
 it 'Should get the /another route' do
-    get '/schedule' 
+    get '/schedule'
     expect(last_response).to be_ok
     expect(last_response.body).to include('We have a lot to do today!')
 end
 
 it 'Should get the /dinner route' do
-    get '/dinner' 
+    get '/dinner'
     expect(last_response).to be_ok
     expect(last_response.body).to include('Dinner Bell! Dinnnnngggg')
 end
