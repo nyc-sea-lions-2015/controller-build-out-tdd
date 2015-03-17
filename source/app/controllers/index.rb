ad167bb2f1f3bcac95c197ec0d229dde378fc45c
Dan Asselin & Eveanandi Butler <pair+danasselin.ebutler90@devbootcamp.com>
2015-03-17 10:59:03 -0400

    Revise index_2.html and create route in index.rb to get to /schedule

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -3,3 +3,7 @@ get '/' do
   # Above we added path parameters '..' to find the right directory to open
   # File.read('../../public/index.html')
 end
+
+get '/schedule' do
+  File.read(File.join('..', '..', 'public', 'index_2.html'))
+end
