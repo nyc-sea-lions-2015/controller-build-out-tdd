ece78a8a42725c7466ceacd4eea44e4e992585b4
Angel Baek & Stephanie Lo <pair+gellieb.steppinlo@devbootcamp.com>
2015-03-17 10:26:16 -0400

    finished up to Release 1

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,3 +1,20 @@
 get '/' do
-  File.read(File.join('public','index.html')) 
+  #File.read(File.join('public','index.html'))
+  'Welcome to Pier 39'
+end
+
+post '/another' do
+  redirect '/schedule'
+end
+
+get '/schedule' do
+  "We have a lot to do today!"
+end
+
+get '/dinner' do
+  'Dinner Bell! Dinnnnngggg'
+end
+
+post '/try_a_post' do
+  redirect '/'
 end
