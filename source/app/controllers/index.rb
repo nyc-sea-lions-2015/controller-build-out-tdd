75818938a954046d065817a2fcfdc6409f5bda2f
Malcolm Bouzi & Kiran <pair+mbouzi.phanendar@devbootcamp.com>
2015-03-17 10:50:47 -0400

    complete up to release 1

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,3 +1,15 @@
 get '/' do
-  File.read(File.join('public','index.html')) 
+  File.read(File.join('public','index.html'))
+end
+
+get '/index_2' do
+  File.read(File.join('public','index_2.html'))
+end
+
+get '/index_3' do
+  File.read(File.join('public','index_3.html'))
+end
+
+post '/try_a_post' do
+  redirect '/'
 end
