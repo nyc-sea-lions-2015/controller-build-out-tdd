7df78da4fe20efab40445011d8aaecddca59a0ae
Awesome Ninja Fighter & Tracy Teague <pair+samguergen.tracyteague@devbootcamp.com>
2015-03-17 11:41:30 -0400

    Add updated index files.

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,3 +1,26 @@
 get '/' do
-  File.read(File.join('public','index.html')) 
+  File.read(File.join('..', '..', 'public','index.html'))
+end
+
+get '/schedule' do
+  File.read(File.join('..', '..', 'public','index_2.html'))
+end
+
+get '/dinner' do
+  File.read(File.join('..', '..', 'public','index_3.html'))
+end
+
+post '/try_a_post' do
+  params
+  redirect '/'
+end
+
+put '/try_a_put' do
+  params
+  redirect '/'
+end
+
+delete '/try_a_delete' do
+  params
+  redirect '/'
 end
