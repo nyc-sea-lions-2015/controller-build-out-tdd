198d653c972e7a6d3e469150b617a60aeb5c0572
Natalia Bryzhatenko & suprfrye <pair+notika314.suprfrye@devbootcamp.com>
2015-03-17 10:40:36 -0400

    Create spec for second index

--- source/spec/controllers/index_spec.rb
+++ source/spec/controllers/index_spec.rb
@@ -7,3 +7,11 @@ describe 'IndexController' do
     expect(last_response.body).to include('Hello from index!')
   end
 end
+
+describe 'Index2Controller' do
+  it 'should process the get /index2 route' do
+    get '/index2'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Hello from second index!')
+  end
+end
